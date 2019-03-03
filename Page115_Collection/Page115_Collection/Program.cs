7c9b1cf6d1f13e5f86095c9823bae46087fe6256
junhong cheon <cjswnsghd12@gmail.com>
2019-03-03 17:19:23 +0900

    Page115_Collection

---
+++ Page115_Collection/Page115_Collection/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Page115_Collection
+{
+    class Program
+    {
+        static void Main()
+        {
+            ArrayList onj = new ArrayList();
+            //List<string> onj = new List<string>();
+            onj.Add("Onj");
+            onj.Add("OracleJava");
+            onj.Add("Community");
+
+            Console.WriteLine("onj List");
+            Console.WriteLine("     Count:     {0}", onj.Count);
+            Console.WriteLine("     Capacity:  {0}", onj.Capacity);
+            Console.Write(" onj  Values:");
+            PrintValues(onj);
+            ArrayList onj2 = (ArrayList)onj.Clone();
+            Console.Write(" onj2 Values:");
+            PrintValues(onj2);
+        }
+        public static void PrintValues(IEnumerable myList)
+        {
+            foreach (Object obj in myList)
+            {
+                Console.Write("    {0}", obj);
+                Console.WriteLine();
+            }
+        }
+    }
+}
