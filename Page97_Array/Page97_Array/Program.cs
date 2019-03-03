640b87094280acb58edb7246ac7e299948a42748
junhong cheon <cjswnsghd12@gmail.com>
2019-03-03 15:56:57 +0900

    수업과제

--- Page97_Array/Page97_Array/Program.cs
+++ Page97_Array/Page97_Array/Program.cs
@@ -54,17 +54,23 @@ namespace Page97_Array
         static void Main(string[] args)
         {
             // 1차원배열 (numbers).
-            int[] n1 = new int[3] { 2, 4, 6 }; int[] n2 = new int[] { 2, 4, 6 }; int[] n3 = { 2, 4, 6 };
+            int[] n1 = new int[3] { 2, 4, 6 };
+            int[] n2 = new int[] { 2, 4, 6 };
+            int[] n3 = { 2, 4, 6 };
             ArrDisplay(n1, n2, n3);
+            
             // 1차원배열 (strings).
             string[] s1 = new string[3] { "onj1", "oraclejava1", "onjoracle1" };
             string[] s2 = new string[] { "onj2", "oraclejava2", "onjoracle2" };
             string[] s3 = { "onj3", "oraclejava3", "onjoracle3" };
             ArrDisplay(s1, s2, s3);
+            
             // 다차원배열
-            int[,] n4 = new int[3, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 } }; int[,] n5 = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            int[,] n4 = new int[3, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            int[,] n5 = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
             int[,] n6 = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
             ArrDisplay(n4); ArrDisplay(n5); ArrDisplay(n6);
+            
             // 가변길이 배열
             int[][] n7 = new int[2][] { new int[] { 2, 4, 6 }, new int[] { 1, 3, 5 } };
             int[][] n8 = new int[][] { new int[] { 2, 4, 6 }, new int[] { 1, 3, 5 } };
---
+++ _LAB/_LAB/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //과제1
+            //가변배열, 처음방에는 1,2 두번째방에는 1,2,3 세번째방에는 1,2,3,4
+            int[][] a = new int[][]
+            {
+                new int[] {1,2},
+                new int[] {1,2,3 },
+                new int[] {1,2,3,4}
+            };
+
+            //3행 2열, 이차원배열 1행은 (1,2), 2행은 (3,4), 3행은 (5,6)
+            int[,] b = new int[2, 3] { { 1, 3, 4 }, { 2, 4, 6 } };
+
+            //가변배열 출력
+            foreach (int[] i in a)
+            {
+                foreach (int j in i)
+                {
+                    Console.Write(j + " ");
+                }
+            }
+            Console.WriteLine("\n---------------\n");
+
+            //이차원 배열 출력
+            foreach (int i in b)
+            {
+                Console.Write(i + " ");
+            }
+
+
+            //과제2
+            //----4행2열
+            int[,] twoDim = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+
+            for (int i = 0; i < twoDim.GetLength(0); i++)
+            {
+                for (int j = 0; j < twoDim.GetLength(1); j++)
+                {
+                    Console.Write(twoDim[i, j]);
+                }
+            }
+            Console.WriteLine();
+
+            foreach (int i in twoDim)
+            {
+                Console.Write(i);
+            }
+        }
+    }
+}
