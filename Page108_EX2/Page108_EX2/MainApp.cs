ae95355b068ceea9049eef5c7d0a1134aae3b7a6
junhong cheon <cjswnsghd12@gmail.com>
2019-03-03 16:43:38 +0900

    Page108_EX2

---
+++ Page108_EX2/Page108_EX2/MainApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page108_EX2
+{
+    class MainApp
+    {
+        static T[] GetArray<T>(int size, T val)
+        {
+            T[] array = new T[size];
+            for(int i = 0; i < size; i++)
+            {
+                array[i] = val;
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            string[] str_array = GetArray<string>(3, "오라클자바커뮤니티, 오라클자바커뮤니티");
+            foreach(string s in str_array)
+            {
+                Console.WriteLine(s);
+            }
+            int[] int_array = GetArray<int>(3, 999);
+            foreach(int i in int_array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
