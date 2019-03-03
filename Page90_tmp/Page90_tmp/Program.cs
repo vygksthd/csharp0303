b2d8ee2ffac8dbb597c6a1b3cf5fcb9db6ad9de3
junhong cheon <cjswnsghd12@gmail.com>
2019-03-03 14:36:41 +0900

    Page90_tmp

---
+++ Page90_tmp/Page90_tmp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        //static void Swap(ref int a, ref int b)
+        //{
+        //    int tmp = a; a = b; b = tmp;
+        //}
+        static void Swap(out int a, out int b)
+        {
+            a = 10; b = 20;
+            int tmp = a; a = b; b = tmp;
+        }
+        static void Main(string[] args)
+        {
+            //int a = 10; int b = 20;
+            //Console.WriteLine("a={0}, b={1}", a, b);
+            //Swap(ref a, ref b);
+            //Console.WriteLine("a={0}, b={1}", a, b);
+
+            int a, b;
+            Swap(out a, out b);
+            Console.WriteLine("a={0}, b={1}", a, b);
+        }
+    }
+}
