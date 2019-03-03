97b45e0911a28446904569c854c5d01e77ef6208
junhong cheon <cjswnsghd12@gmail.com>
2019-03-03 16:03:08 +0900

    Page103_깊은복사/얕은복사

---
+++ Page103_깊은복사_얕은복사/Page103_깊은복사_얕은복사/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page103_깊은복사_얕은복사
+{
+    class CreditCard
+    {
+        public string name;
+    }
+    class Customer
+    {
+        public int age;
+        public CreditCard card;
+        public object ShallowCopy()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            c1.age = 20;
+            c1.card = new CreditCard();
+            c1.card.name = "비씨카드";
+
+            Customer c2 = (Customer)c1.ShallowCopy();
+            c2.card.name = "BC카드";
+            Console.WriteLine("c1.card.name = " + c1.card.name);
+            Console.WriteLine("c2.card.name = " + c2.card.name);
+        }
+    }
+}
